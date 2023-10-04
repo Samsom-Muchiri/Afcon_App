192078bf4e2c0c8bf775fd787e19d71db29bc636
Sam .M <you@example.com>
2023-10-04 06:33:21 +0300

    added payment methods to ticket page

--- src/Components/TicketStore.jsx
+++ src/Components/TicketStore.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../Style Sheets/tickets.css";
 import TicketHeader from "./ticket components/TicketHeader";
 import Packages from "./ticket components/Packages";
+import ListTickets from "./ticket components/ListTickets";
+import { Paypal } from "./ticket components/Pay";
 
 function TicketStore() {
   return (
     <>
       <TicketHeader />
       <Packages />
+      <ListTickets />
+      <Paypal />
     </>
   );
 }
